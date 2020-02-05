5129675ee70353bd959c7ad796b2aa99c3853cd9
Kuanisinajar <superbenben@livemail.tw>
2020-02-05 19:07:12 +0800

    [Add] Titled Container
    基於首頁多次用到相同的有標題的 Container，製作一個可重複利用，只需傳入 title 值的 component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 // components
 import PageHeader from "./components/pageHeader";
 import Carousel from "./components/carousel";
-import Block from "./components/block";
+import Feature from './components/feature';
 
 // data
 import headerData from "./json/pageHeader";
@@ -14,6 +14,7 @@ function App() {
         <div className="App">
             <PageHeader data={headerData} />
             <Carousel data={carouselData} />
+            <Feature />
         </div>
     );
 }
---
+++ src/components/feature.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import Block from './block';
+import TitledContainer from './titledContainer';
+
+const Feature = () => {
+    return ( 
+        <Block id='feature'>
+            <TitledContainer title="這看起來超酷！">
+                
+            </TitledContainer>
+        </Block>
+     );
+}
+ 
+export default Feature;

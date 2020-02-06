cc67d0932cd92b44878dc89ea69ae0e1c87bfa8a
Kuanisinajar <superbenben@livemail.tw>
2020-02-06 10:03:01 +0800

    [Add] CourseCard component, container and imgContainer
    Container 與 imgContainer 皆使用 padding-top 來決定元素高度

---
+++ src/components/courseCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CourseCard = () => {
+    return (
+        <div className="courseCard">
+            <div className="courseCard_wrapper">
+                <div className="courseCard_imgContainer">
+                    <img src="https://5xruby.tw/assets/images/talks/cover/functional-09be0f61.jpg" alt=""/>
+                </div>
+                <div className="courseCard_contentWrapper">
+                    <div className="courseCard_tagWrapper">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CourseCard;
--- src/components/popularCourse.js
+++ src/components/popularCourse.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
 
 // components
-import Block from './block';
-import TitledContainer from './titledContainer';
+import Block from "./block";
+import TitledContainer from "./titledContainer";
+import CourseCard from "./courseCard";
 
 const PopularCourse = () => {
-    return ( 
-        <Block id='popularCourse'>
+    return (
+        <Block id="popularCourse">
             <TitledContainer title="熱門網頁設計課程推薦">
-
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-4">
+                        <CourseCard />
+                    </div>
+                </div>
             </TitledContainer>
         </Block>
-     );
-}
- 
+    );
+};
+
 export default PopularCourse;

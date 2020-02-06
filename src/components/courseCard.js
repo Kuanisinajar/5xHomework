615714040610e14e616158dbe83bf2e000e20dbd
Kuanisinajar <superbenben@livemail.tw>
2020-02-06 10:25:44 +0800

    [Add] CourseCard_content UI

--- src/components/courseCard.js
+++ src/components/courseCard.js
@@ -9,7 +9,14 @@ const CourseCard = () => {
                 </div>
                 <div className="courseCard_contentWrapper">
                     <div className="courseCard_tagWrapper">
-                        
+                        <span className="tag tag_newCourse">新開課</span>
+                        <span className="tag tag_opening">開放報名中</span>
+                        <h3>工作上用得到的函數式程式設計：從觀念到實務 - 假日班</h3>
+                        <small>講師：蘇泰安(Taian Su)</small>
+                        <div className="courseCard_timeInfo">
+                            <span>開課時間</span>
+                            <span>2月｜假日班</span>
+                        </div>
                     </div>
                 </div>
             </div>

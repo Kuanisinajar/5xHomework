da4bb7d3b2b733fc3c3ae1bc80e922b0c4a14873
Kuanisinajar <superbenben@livemail.tw>
2020-02-06 14:56:23 +0800

    [Add] Showcase component structure

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Carousel from "./components/carousel";
 import Feature from "./components/feature";
 import PopularCourse from "./components/popularCourse";
 import Feedback from "./components/feedback";
+import Showcase from './components/showcase';
 
 // data
 import headerData from "./json/pageHeader";
@@ -13,6 +14,7 @@ import carouselData from "./json/carousel";
 import featureData from "./json/feature";
 import popularCourseData from "./json/popularCourse";
 import feedbackData from './json/feedback';
+import showcaseData from './json/showcase';
 
 function App() {
     return (
@@ -33,6 +35,7 @@ function App() {
             <Feature data={featureData} />
             <PopularCourse data={popularCourseData} />
             <Feedback data={feedbackData}/>
+            <Showcase data={showcaseData}/>
         </div>
     );
 }
---
+++ src/components/showcase.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// components
+import Block from './block';
+import TitledContainer from './titledContainer';
+
+const Showcase = () => {
+    return ( 
+        <Block id='showcase'>
+            <TitledContainer title='案例作品 Showcase'>
+
+            </TitledContainer>
+        </Block>
+     );
+}
+ 
+export default Showcase;

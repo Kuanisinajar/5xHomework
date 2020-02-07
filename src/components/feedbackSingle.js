cb9529d7b3056892f3c54a91dbaf6a4f8a5346a5
Kuanisinajar <superbenben@livemail.tw>
2020-02-07 17:47:20 +0800

    [Modify] Feedback refactor
    1. 更改 feedbackSingle UI
    2. 更動 RWD 中 feedbackSingle 的container 大小

--- src/components/feedbackSingle.js
+++ src/components/feedbackSingle.js
@@ -5,15 +5,23 @@ const FeedbackSingle = props => {
     return (
         <div className="container-fluid feedbackSingle">
             <div className="row justify-content-center align-items-center feedbackSingle_row">
-                <div className="col-2 col-sm-4 col-md-2 col-lg-2 feedbackSingle">
-                    <div className="feedbackSingle_imgWrapper">
-                        <img src={img_url} alt="" />
+                <div className="col-12 col-sm-10 col-md-8 col-lg-6 container">
+                    <div className="row">
+                        <div className="col-12 feedbackSingle_feedback">
+                            <p>{feedback}</p>
+                        </div>
+                        <div className="col-3 col-sm-2 feedbackSingle_img">
+                            <div className="feedbackSingle_imgWrapper">
+                                <img src={img_url} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-9 col-sm-10 feedbackSingle_person">
+                            <h1>{person}</h1>
+                            <h2>
+                                {identity}
+                            </h2>
+                        </div>
                     </div>
-                </div>
-                <div className="col-6 col-sm-8 col-md-6 col-lg-6">
-                    <p className="feedbackSingle_feedback">{feedback}</p>
-                    <h3 className="feedbackSingle_person">{person}</h3>
-                    <h6 className="feedbackSingle_identity">{identity}</h6>
                 </div>
             </div>
         </div>

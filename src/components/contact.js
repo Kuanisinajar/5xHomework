5313f6e0ffd9ea5696bfed72ccf2d61a1200a61d
Kuanisinajar <superbenben@livemail.tw>
2020-02-08 21:58:40 +0800

    [Add] Contact form style
    1. 使用 bootstrap grid 編排
    2. 使用 psudo class 做不同狀態之樣式

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,22 +10,82 @@ class Contact extends Component {
                 </header>
                 <div className="container">
                     <div className="row justify-content-center">
-                        <div className="col-10">
-                            <form action="">
-                                <input type="text" name="name" placeholder='名字' required />
-                                <input type="email" name="email" placeholder='信箱' required/>
-                                <input type="text" name="phone" placeholder='電話' required/>
-                                <select name="topic" required defaultValue="default">
-                                    <option value="default" disabled hidden>
-                                        請選擇主題
-                                    </option>
-                                    <option value="project">專案開發</option>
-                                    <option value="technical">技術諮詢</option>
-                                    <option value="coporation">企業內訓</option>
-                                    <option value="course">課程詢問</option>
-                                    <option value="other">其他</option>
-                                </select>
-                                <textarea name="message" id="" cols="30" rows="10" required></textarea>
+                        <div className="col-12 contactForm_wrapper">
+                            <form
+                                action=""
+                                className="contactForm container-fluid"
+                            >
+                                <div className="row">
+                                    <label className="col-12 col-md-6">
+                                        您的名字
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            placeholder="名字"
+                                            required
+                                        />
+                                    </label>
+                                    <label className="col-12 col-md-6">
+                                        您的信箱
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            placeholder="信箱"
+                                            required
+                                        />
+                                    </label>
+                                    <label className="col-12 col-md-6">
+                                        您的電話
+                                        <input
+                                            type="text"
+                                            name="phone"
+                                            placeholder="電話"
+                                            required
+                                        />
+                                    </label>
+                                    <label className="col-12 col-md-6">
+                                        詢問主題
+                                        <select
+                                            name="topic"
+                                            defaultValue="default"
+                                            required
+                                        >
+                                            <option
+                                                value="default"
+                                                disabled
+                                                hidden
+                                            >
+                                                請選擇主題
+                                            </option>
+                                            <option value="project">
+                                                專案開發
+                                            </option>
+                                            <option value="technical">
+                                                技術諮詢
+                                            </option>
+                                            <option value="coporation">
+                                                企業內訓
+                                            </option>
+                                            <option value="course">
+                                                課程詢問
+                                            </option>
+                                            <option value="other">其他</option>
+                                        </select>
+                                    </label>
+                                    <label className="col-12">
+                                        您的訊息
+                                        <textarea
+                                            name="message"
+                                            id=""
+                                            cols="30"
+                                            rows="10"
+                                            placeholder="請留下您的訊息"
+                                            required
+                                        ></textarea>
+                                    </label>
+                                </div>
+
+                                <button>送出</button>
                             </form>
                         </div>
                     </div>

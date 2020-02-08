880b2cdae8d50ee235e12811a1aa1b6f053482b5
Kuanisinajar <superbenben@livemail.tw>
2020-02-08 21:14:22 +0800

    [Add] Contact form markup

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,6 +8,28 @@ class Contact extends Component {
                 <header className="contacts_header">
                     <h1>有任何問題嗎？馬上聯絡我們</h1>
                 </header>
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-10">
+                            <form action="">
+                                <input type="text" name="name" placeholder='名字' required />
+                                <input type="email" name="email" placeholder='信箱' required/>
+                                <input type="text" name="phone" placeholder='電話' required/>
+                                <select name="topic" required defaultValue="default">
+                                    <option value="default" disabled hidden>
+                                        請選擇主題
+                                    </option>
+                                    <option value="project">專案開發</option>
+                                    <option value="technical">技術諮詢</option>
+                                    <option value="coporation">企業內訓</option>
+                                    <option value="course">課程詢問</option>
+                                    <option value="other">其他</option>
+                                </select>
+                                <textarea name="message" id="" cols="30" rows="10" required></textarea>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </section>
         );
     }

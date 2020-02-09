719598bdbd5233b5f16a0b23ba20c887ab830b2f
Kuanisinajar <superbenben@livemail.tw>
2020-02-09 13:12:25 +0800

    [Add] Router
    1. 使用 demoRoute 作為每個 header項目的模板
    2. 建立的 route: course, talks, dev, training, space, posts
    3. 改用 HashRouter 以適應沒有後端腳本的狀況

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 // components
 import PageHeader from "./components/pageHeader";
 import Main from "./components/main";
 import Contact from './components/contact';
 import PageFooter from "./components/pageFooter";
+import Course from './components/course';
+import Talks from './components/talks';
+import Dev from './components/dev';
+import Training from './components/training';
+import Space from './components/space';
+import Posts from './components/posts';
+
 
 // data
 import headerData from "./json/pageHeader";
@@ -14,12 +21,18 @@ import footerData from "./json/footer";
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <HashRouter>
                 <PageHeader data={headerData} />
                 <Route exact path='/' component={ Main } />
+                <Route path='/course' component={ Course } /> 
+                <Route path='/talks' component={ Talks } />
+                <Route path='/dev' component={ Dev } />
+                <Route path='/training' component={ Training } />
+                <Route path='/space' component={ Space } />
+                <Route path='/posts' component={ Posts } />
                 <Route path='/contacts' component={ Contact } />
                 <PageFooter data={footerData} />
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
